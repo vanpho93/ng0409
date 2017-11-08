6af976bf3bbef5b22f9da764e1781ea286dda1ec
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-08 18:20:20 +0700

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+// const { sign } = require('jsonwebtoken');
 
+// decorator function
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Khoa Pham';
+  company = {
+    name: 'KPT Training',
+    address: '92 LRT'
+  };
 }
+
+// module.exports = { AppComponent };
