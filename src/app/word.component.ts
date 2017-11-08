8795581031b13dc273dbd2196a740888bd0b4ba5
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-08 18:28:01 +0700

    tao component

---
+++ src/app/word.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-word',
+    template: `
+        <h3>Word Component 1123s</h3>
+    `
+})
+
+export class WordComponent {}
