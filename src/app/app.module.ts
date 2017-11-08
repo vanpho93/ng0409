9123740b5a5a597cc702d49d6a056726baecc5a2
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-08 18:38:51 +0700

    abcd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './word.component';
+import { ListWordsComponent } from './list-words/list-words.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordComponent
+    WordComponent,
+    ListWordsComponent
   ],
   imports: [
     BrowserModule
